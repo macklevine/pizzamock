57a7a723946d293f03cca219b4ffa8173d251d56
Mack Levine <mlevine@administorsmbp3.attlocal.net>
2016-07-06 21:01:31 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var http = require('http');
+
+var app = express();
+var server;
+
+server.listen(1337, function(){
+	console.log('lead exporter listening on port 1337');
+});
